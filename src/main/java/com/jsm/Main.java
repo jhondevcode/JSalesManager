e36794ef646ba1640855bb5b97dec95942a8ef38
Jhon Fernandez <jhondev.code@gmail.com>
2021-05-23 23:33:52 -0500

    Description of the App.java class

--- src/main/java/com/jsm/Main.java
+++ src/main/java/com/jsm/Main.java
@@ -3,15 +3,28 @@ package com.jsm;
 import com.jsm.log.LoggerConfigurator;
 import org.tinylog.Logger;
 
+/**
+ * This class has the simple task of configuring the loger and launching the
+ * program.
+ * 
+ * @author jhondev-code
+ * @version 1.0.0
+ */
 public class Main {
 
-    static {
-        LoggerConfigurator.configure();
-    }
+	/**
+	 * Set up the logger
+	 */
+	static {
+		LoggerConfigurator.configure();
+	}
 
-    public static void main(String[] args) {
-        Logger.info("Starting...");
-        JSMApp.start(args);
-    }
+	/**
+	 * Launch the program
+	 */
+	public static void main(String[] args) {
+		Logger.info("Starting...");
+		JSMApp.start(args);
+	}
 
 }
