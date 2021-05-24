e338acb883e032615ffb958e45dadd03219e3822
Jhon Fernandez <jhondev.code@gmail.com>
2021-05-24 09:54:25 -0500

    I removed the test code

--- src/main/java/com/jsm/JSMApp.java
+++ src/main/java/com/jsm/JSMApp.java
@@ -23,22 +23,6 @@ public class JSMApp {
 	 * configuring and displaying the program.
 	 */
 	public static void start(String[] args) {
-		Image spotify = null;
-		try {
-			spotify = SVGLoader.toImage("/img/svg/spotify.svg", 64, 64);
-		} catch (TranscoderException e) {
-			e.printStackTrace();
-		}
-		if (spotify != null) {
-			JFrame frame = new JFrame("Hello");
-			frame.setSize(200, 200);
-			frame.setLocationRelativeTo(null);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setIconImage(spotify);
-			frame.setVisible(true);
-		} else {
-			System.out.println("Error: unrecognized image");
-		}
 	}
 
 }
