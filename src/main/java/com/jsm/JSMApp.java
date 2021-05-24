3c64239d8a0a60407f4c9da391a6ce562cd1d2b9
Jhon Fernandez <jhondev.code@gmail.com>
2021-05-23 21:58:26 -0500

    I have added the class that starts the whole application

---
+++ src/main/java/com/jsm/JSMApp.java
@@ -0,0 +1,13 @@
+package com.jsm;
+
+public class JSMApp {
+
+    public static final String PATH_SEPARATOR = System.getProperty("file.separator");
+    public static final String WORKSPACE = System.getProperty("user.home") + PATH_SEPARATOR +  ".jsm";
+    public static final String APP_VERSION = "0.1.0";
+
+    public static void start(String[] args) {
+        // ss
+    }
+
+}
